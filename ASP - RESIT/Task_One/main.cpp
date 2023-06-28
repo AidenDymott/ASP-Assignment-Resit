d3df65350a6c168e87038ff7de87112eb1f1d2d3
AidenDymott <97362961+AidenDymott@users.noreply.github.com>
2023-06-28 14:58:09 +0100

    Initial Commit, Task 1 p1

---
+++ ASP - RESIT/Task_One/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdint>
+
+#include "context.hpp"
+
+
+int main() {
+    volatile int x = 0;
+    Context c;
+
+    get_context(&c);
+
+    std::cout << "a message" << std::endl;
+
+    if (x == 0) {
+        x = x + 1;
+        set_context(&c);
+    }
+    return 0;
+}
